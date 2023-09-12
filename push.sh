d86ee67737f6c063aa69b2bcce9f1b7848400e17
June <street.visions@yahoo.com>
2023-09-12 10:54:58 -0500

    test2

--- push.sh
+++ push.sh
@@ -1,5 +1,34 @@
 #!/bin/bash  
+
+STRING="{\"Themes\": {"
+for f in */*; do
+	declare -a DOWNLOADS=()
+	STRING=$STRING"\"$(basename $f)\":{"
+	for i in $f/*; do
+		N=$(basename $i)
+		if [ "${N: -4}" == ".jpg" ]; 
+		then
+			DOWNLOADS+=(\"$N\")
+			STRING=$STRING"\"img\":\"$N\"," #image
+		elif [[ $N != *.* ]];  #weather icon
+		then
+			DOWNLOADS+=(\"$N/weatherIcon\")
+		else
+			DOWNLOADS+=(\"$N\")
+		fi
+	done
+	STRING=$STRING"\"downloads\":["
+	for d in "${DOWNLOADS[@]}"; do
+		STRING=$STRING$d,
+	done
+	STRING=$STRING"]},"
+done
+STRING=$STRING"}"
+echo $STRING > themes.json
+
 git add .
 git commit -m "$*"
 git branch -M main
 git push -u origin main
+
+
